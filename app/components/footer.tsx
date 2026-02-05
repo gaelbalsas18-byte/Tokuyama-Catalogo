54c635300e51d9fbcf706f331f2ecd240e80dc3d
Gael <gaelbalsas18@gmail.com>
2026-02-05 14:58:57 -0600

    footer responsivo

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,52 +4,35 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-800 text-white py-6 px-8">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4 text-sm ">
+    <footer className="bg-blue-800 text-white py-6 px-6">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6 text-sm">
 
-
-        {/* ===== REDES SOCIALES (IZQUIERDA) ===== */}
+        {/* ===== REDES SOCIALES ===== */}
         <div className="flex items-center gap-5">
           <a href="https://www.facebook.com/BalsasDentalmx" target="_blank">
-            <Image
-              src="/Redes/Facebook.png"
-              alt="Facebook"
-              width={32}
-              height={32}
-            />
+            <Image src="/Redes/Facebook.png" alt="Facebook" width={28} height={28}/>
           </a>
 
           <a href="https://www.instagram.com/balsasdentalmx/" target="_blank">
-            <Image
-              src="/Redes/Instagram.png"
-              alt="Instagram"
-              width={32}
-              height={32}
-            />
+            <Image src="/Redes/Instagram.png" alt="Instagram" width={28} height={28}/>
           </a>
 
-           <a href="https://wa.me/52 55 8449 4688" target="_blank">
-            <Image
-              src="/Redes/Whatsapp.png"
-              alt="WhatsApp"
-              width={32}
-              height={32}
-            />
+          <a href="https://wa.me/525584494688" target="_blank">
+            <Image src="/Redes/Whatsapp.png" alt="WhatsApp" width={28} height={28}/>
           </a>
+        </div>
 
-        </div>  
-          
-
-        {/* ===== TEXTO DERECHA (MISMA LÍNEA) ===== */}
-        <div className="flex items-center gap-6 -translate-x-65">
-          <p className="whitespace-nowrap " >
+        {/* ===== TEXTO LEGAL ===== */}
+        <div className="flex flex-col md:flex-row items-center gap-3 md:gap-6 text-center md:text-right">
+          <p>
             © {new Date().getFullYear()} Balsas Dental · Tokuyama. Todos los derechos reservados.
           </p>
+
           <a
             href="https://balsasdental.com.mx/wp-content/uploads/2025/12/AV-PRIVACIDAD-USO-DE-IMAGEN-Y-C-ATOTIZACION-CLIENTES.pdf"
             target="_blank"
             rel="noopener noreferrer"
-            className="underline underline-offset-4 hover:text-white whitespace-nowrap"
+            className="underline underline-offset-4 hover:text-white"
           >
             Aviso de privacidad
           </a>
